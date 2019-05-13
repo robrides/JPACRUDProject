ad3c1d579d4dadc40337c843f550db6c59dd3bf8
robrides <techjunkey@gmail.com>
2019-05-13 10:33:26 -0600

    Modified Gradle to add war plugin. Created .war file

--- UserMVCBoot/src/main/java/com/skilldistillery/users/UserMvcBootApplication.java
+++ UserMVCBoot/src/main/java/com/skilldistillery/users/UserMvcBootApplication.java
@@ -3,10 +3,16 @@ package com.skilldistillery.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EntityScan("com.skilldistillery.users")
-public class UserMvcBootApplication {
+public class UserMvcBootApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(UserMvcBootApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserMvcBootApplication.class, args);
